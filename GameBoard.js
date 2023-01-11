5e7cf2fb8faaa89f507c2b38a46962d8eaadb150
essohanam <essohanam.alou@imsp-uac.org>
2023-01-11 08:12:09 +0000

    GameBoard with successfull test

--- GameBoard.js
+++ GameBoard.js
@@ -1,7 +1,42 @@
+import Ship from "./Ship.js";
 class GameBoard {
     constructor() { };
+    missedShots = [];
+    sunkShipsNumber = 0;
     placeShipAt(Ship, coordinates) {
         Ship.coordinates = coordinates;
+    }
+
+    receiveAttack(attack, Ships) {
+        const shipsCoordinates = this.getStringifyedCoordinatesOf(Ships);
+        let attackHit = false;
+        let hitshipIndex;
+        shipsCoordinates.forEach((strshipCoordinates, index) => {
+            const shipCoordinates = JSON.parse(strshipCoordinates)
+                .map(keyVal => JSON.stringify(keyVal)); // stringify each pair
+            if (shipCoordinates.includes(JSON.stringify(attack))) {
+                attackHit = true;
+                hitshipIndex = index;
+            }
+        })
+        if (attackHit) {
+            Ships[hitshipIndex].hit();
+            if (Ships[hitshipIndex].isSunk()) {
+                this.sunkShipsNumber += 1;
+            }
+        } else {
+            this.missedShots.push(JSON.stringify(attack));
+        }
+
+    }
+
+    getStringifyedCoordinatesOf(allShips) {
+        const shipsCoordinates = [];
+        for (let i = 0; i < allShips.length; i += 1) {
+            const coordinates = JSON.stringify(allShips[i].coordinates); //stringify each array of coordinates to avoid array of arrays
+            shipsCoordinates.push(coordinates);
+        }
+        return shipsCoordinates;
     }
 }
 
--- GameBoard.test.js
+++ GameBoard.test.js
@@ -1,28 +1,63 @@
 import GameBoard from "./GameBoard.js";
 import Ship from "./Ship.js";
 
+const myGameBoard = new GameBoard();
+const twoLengthShip = Ship(2);
+const oneLengthShip = Ship(1);
+const threeLengthShip = Ship(3);
+const firstCoordinates = [{ f: 2 }];
+const secondCoordinates = [{ f: 5 }, { f: 6 }];
+const thirdCoordinates = [{ a: 1 }, { b: 1 }, { c: 2 }];
+const missedShots = myGameBoard.missedShots;
+myGameBoard.placeShipAt(oneLengthShip, firstCoordinates);
+myGameBoard.placeShipAt(twoLengthShip, secondCoordinates);
+myGameBoard.placeShipAt(threeLengthShip, thirdCoordinates);
+
+const ships = [
+    oneLengthShip
+    , twoLengthShip
+    , threeLengthShip
+]
+
 test('GameBoard place a ship at a specific  coordinate', () => {
-    const twoLengthShip = Ship(2);
-    const oneLengthShip = Ship(1);
-    // const fourLengthShip = Ship(4);
-    const threeLengthShip = Ship(3);
 
-    const firstCoordinates = [JSON.stringify({ f: 2 })];
-    const secondCoordinates = [
-        JSON.stringify({ "f": 5 })
-        , JSON.stringify({ f: 6 })
-    ];
-    const thirdCoordinates = [
-        JSON.stringify({ a: 1 })
-        , JSON.stringify({ b: 1 })
-        , JSON.stringify({ c: 2 })
-    ];
-    const myGameBoard = new GameBoard();
-    myGameBoard.placeShipAt(oneLengthShip, firstCoordinates);
-    myGameBoard.placeShipAt(twoLengthShip, secondCoordinates);
-    myGameBoard.placeShipAt(threeLengthShip, thirdCoordinates);
+    expect(JSON.stringify(oneLengthShip.coordinates))
+        .toEqual(JSON.stringify(firstCoordinates));
+    expect(JSON.stringify(twoLengthShip.coordinates))
+        .toEqual(JSON.stringify(secondCoordinates));
+    expect(JSON.stringify(threeLengthShip.coordinates))
+        .toEqual(JSON.stringify(thirdCoordinates));
+});
 
-    expect(oneLengthShip.coordinates).toEqual(firstCoordinates);
-    expect(twoLengthShip.coordinates).toEqual(secondCoordinates);
-    expect(threeLengthShip.coordinates).toEqual(thirdCoordinates);
-}) 
+test('getstringifyedCoordinatesOf return correct structure', () => {
+    expect(myGameBoard.getStringifyedCoordinatesOf(ships))
+        .toEqual([JSON.stringify(firstCoordinates), JSON.stringify(secondCoordinates),
+        JSON.stringify(thirdCoordinates)]);
+})
+const attack = { b: 1 };
+const seconAttack = { f: 2 };
+const thirdAttack = { c: 2 };
+const missedAttack = { d: 0 };
+const lastAttak = { a: 1 };
+myGameBoard.receiveAttack(attack, ships);
+myGameBoard.receiveAttack(seconAttack, ships);
+myGameBoard.receiveAttack(thirdAttack, ships);
+myGameBoard.receiveAttack(missedAttack, ships);
+myGameBoard.receiveAttack(lastAttak, ships);
+test('GameBoard receiveAttack determine whether or not the attack hit a ship',
+    () => {
+        expect(threeLengthShip.numberOfHits).toBe(3);
+        expect(missedShots.includes(JSON.stringify(attack))).toBeFalsy();
+        expect(missedShots.length).toBe(1);
+        expect(missedShots.includes(JSON.stringify(missedAttack))).toBeTruthy();
+        expect(oneLengthShip.numberOfHits).toBe(1);
+        expect(twoLengthShip.numberOfHits).toBe(0);
+    }
+)
+
+test('GameBoard report the number of sunk ships', () => {
+    expect(oneLengthShip.isSunk()).toBeTruthy();
+    expect(threeLengthShip.isSunk()).toBeTruthy();
+    expect(twoLengthShip.isSunk()).toBeFalsy();
+    expect(myGameBoard.sunkShipsNumber).toBe(2);
+})
